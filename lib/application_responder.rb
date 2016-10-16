bcf7a38d9f56ff8ceec09c1086a2d7a6be57c713
Roman Kolesnev <rvkolesnev@gmail.com>
2016-10-16 16:55:41 +0300

    CSS cleanup

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -1,6 +1,6 @@
 class ApplicationResponder < ActionController::Responder
   include Responders::FlashResponder
-  # include Responders::HttpCacheResponder
+  include Responders::HttpCacheResponder
 
   # Redirects resources to the collection path (index action) instead
   # of the resource path (show action) for POST/PUT/DELETE requests.
