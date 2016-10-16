426005e1cc5bc5a783e620a6061ba4e573ef749e
Roman Kolesnev <rvkolesnev@gmail.com>
2016-10-16 12:45:25 +0300

    Basic loans scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'home#home'
+  root to: 'loans#index'
+
+  resources :loans
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
